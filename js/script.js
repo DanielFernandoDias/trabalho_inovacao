cff7cf27ecf53f26c222207d04eadc94b85d5f93
Daniel Dias <danielfernando9101@gmail.com>
2022-09-10 16:10:55 -0300

    first

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function aparecerTexto() {
+    document.getElementById("calabresa").innerHTML = "violencia fisica";
+    document.getElementById("portuguesa").innerHTML = "violencia sexual";
+    document.getElementById("queijos").innerHTML = "violencia psicologica";
+};
+  function reset() {
+    document.getElementById("calabresa").innerHTML = "Sabor Calabresa";
+    document.getElementById("portuguesa").innerHTML = "Sabor portuguesa";
+    document.getElementById("queijos").innerHTML = "Sabor 4 queijos";
+};
